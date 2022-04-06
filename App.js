4fae65dc42bcfc4f4c6dfcf501b1b211ba33b8d9
unknown <adrie1stpentatonic@gmail.com git config --global user.name adrieSuryaKgit commit -m>
2022-04-06 14:01:22 +0700

    contoh functional component

--- App.js
+++ App.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import {Text, View, Image, TextInput} from 'react-native';
 
-
 const App = () => {
   return (
-<View>
-  <View style={{width: 100, height:100, backgroundColor:'#686de0'}}/>
-  <Text>Tes 01</Text>
-  <Text>Tes 02</Text>
-  <Text>Tes 03</Text>
-  <Coba/>
-  <Foto/>
-  <TextInput style={{borderWidth:1}} />
-</View>
+    <View>
+      <View style={{width: 100, height: 100, backgroundColor: '#686de0'}} />
+      <Text>Tes 01</Text>
+      <Text>Tes 02</Text>
+      <Text>Tes 03</Text>
+      <Coba />
+      <Text>Tes 05</Text>
+      <Foto />
+      <TextInput style={{borderWidth: 1}} />
+    </View>
   );
 };
 
+// functional component
+
 const Coba = () => {
   return <Text>Tes 04 dari const coba</Text>;
-}
+};
 
 const Foto = () => {
-  return <Image source={{uri: 'https://placeimg.com/100/100/nature'}} style={{width:100, height:100}} />
-}
+  return (
+    <Image
+      source={{uri: 'https://placeimg.com/100/100/nature'}}
+      style={{width: 100, height: 100}}
+    />
+  );
+};
 
 export default App;
