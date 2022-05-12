544a6515d4bb70608955077f9d8a16b01db511e2
unknown <adrie1stpentatonic@gmail.com git config --global user.name adrieSuryaKgit commit -m>
2022-05-12 17:17:39 +0700

    memunculkan hasil call api ke aplikasi

--- src/pages/CallAPIVanilaJS/index.js
+++ src/pages/CallAPIVanilaJS/index.js
@@ -1,37 +1,91 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, {useEffect} from 'react'
+import { StyleSheet, Text, View, Button, Image } from 'react-native'
+import React, {useEffect, useState} from 'react'
 
 const CallAPIVanilaJS = () => {
-  useEffect(()=>{
-    //call api method get
+  const[dataUser, setdataUser] = useState({
+    avatar:'',
+    email:'',
+    first_name:'',
+    last_name:'',
+  })
+
+  const [dataJob, setdataJob] = useState({
+    name:'',
+    job:'',
+  })
+
+  // useEffect(()=>{
+  //   //call api method get
+  //   fetch('https://reqres.in/api/users/2')
+  //   .then(response => response.json())
+  //   .then(json => console.log(json))
+  
+
+  // //call api method post
+  // const dataForAPI={
+  //     name: "morpheus",
+  //     job: "leader"
+  // }
+  // console.log('data object: ', dataForAPI);
+  // console.log('data stringify: ', JSON.stringify(dataForAPI));
+  // fetch('https://reqres.in/api/users', {
+  //   method:'POST',
+  //   headers:{
+  //     'Content-Type':'application/json'
+  //   },
+  //   body: JSON.stringify(dataForAPI)
+  // })
+  // .then(response=>response.json())
+  // .then(json=>{
+  //   console.log('post response: ', json)
+  // })
+
+  // },[]);
+
+  const getData = () =>{
     fetch('https://reqres.in/api/users/2')
     .then(response => response.json())
-    .then(json => console.log(json))
-
-  //call api method post
-  const dataForAPI={
-      name: "morpheus",
-      job: "leader"
+    // .then(json => console.log(json))
+    .then(json => {
+      console.log(json)
+      setdataUser(json.data)
+    })
   }
-  console.log('data object: ', dataForAPI);
-  console.log('data stringify: ', JSON.stringify(dataForAPI));
-  fetch('https://reqres.in/api/users', {
-    method:'POST',
-    headers:{
-      'Content-Type':'application/json'
-    },
-    body: JSON.stringify(dataForAPI)
-  })
-  .then(response=>response.json())
-  .then(json=>{
-    console.log('post response: ', json)
-  })
-
-  },[]);
+  
+  const postData =()=>{
+    const dataForAPI={
+          name: "morpheus",
+          job: "leader"
+      }
+      fetch('https://reqres.in/api/users', {
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify(dataForAPI)
+      })
+      .then(response=>response.json())
+      .then(json=>{
+        console.log('post response: ', json)
+        setdataJob(json)
+      })
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.judul}>Call API Dengan Vanila JS</Text>
+      <Button title='GET DATA' color="#ff5c5c" onPress={getData}/>
+      <Text style={styles.judul}>Respon dari GET DATA</Text>
+      {dataUser.avatar.length > 0 && (
+   <Image source={{uri:dataUser.avatar}} style={styles.foto}/>
+      )}
+      <Text>{`${dataUser.first_name} ${dataUser.last_name}`}</Text>
+      <Text>{dataUser.email}</Text>
+      <View style={styles.line}/>
+      <Button title='POST DATA' color="#ff5c5c" onPress={postData}/>
+      <Text style={styles.judul}>Respon dari POST DATA</Text>
+      <Text style={styles.judul}>{dataJob.name}</Text>
+      <Text style={styles.judul}>{dataJob.job}</Text>
     </View>
   )
 }
@@ -43,6 +97,17 @@ const styles = StyleSheet.create({
     padding:20
   },
   judul:{
-    textAlign:'center'
+    textAlign:'center',
+    marginVertical:20
+  },
+  line:{
+    backgroundColor:'black',
+    height:2,
+    marginVertical:20
+  },
+  foto:{
+    width:80,
+    height:80,
+    borderRadius:80
   }
 })
